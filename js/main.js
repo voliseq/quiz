cc84cb6bd33696556dca9ac324a7568fff5ff895
voliseq <maciej.radziszowski@gmail.com>
2017-05-03 20:29:56 +0200

    Adding answers

--- js/main.js
+++ js/main.js
@@ -9,52 +9,71 @@
 
         const init = () => {
 
-            let _time = q_data.time_seconds,
-                _questions = q_data.questions,
-                _points = 0,
-                _current = 0;
-
             let ui_question_text = document.getElementsByClassName("question__text")[0],
                 ui_answers = Array.from(document.getElementsByClassName('answer')),
                 ui_answers_texts = ui_answers.map(x => x.children[0]),
                 prev = document.getElementsByClassName('prev')[0],
                 next = document.getElementsByClassName('next')[0];
 
+            let _time = q_data.time_seconds,
+                _questions = q_data.questions,
+                _points = 0,
+                _current = -1,
+                _user_answers = q_data.questions.map((question) => {
+                    return question.answers;
+                }).map((answers) => {
+                    return answers.filter((answer) => {
+                        return answer.correct.id
+                    })
+                }).map((correct) => {
+                    return correct[0].id
+                }); // arrays of ids of correct answers
+
+
+
+
 
             let changeText = (element, text) => {
                 element.innerText = text;
             };
 
-
-            // let selectAnswer = (id) => {
-            //     _questions[_current].
-            // }
+            let addAnswers = (e) => {
+                let answer = e.target.getAttribute('data-answer-id') || e.target.parentNode.getAttribute('data-answer-id');
+                _user_answers[_current] = answer;
+                console.log(_user_answers);
+            }
 
             let changeQuestion = (direction) => { // 0 - previous 1 - next
-                if(direction && _current < _questions.length){
+                if (direction && _current < _questions.length) {
                     _current++
                 }
-                else if(!direction && _current > 0){
+                else if (!direction && _current > 0) {
                     _current--;
                 }
-                else{
+                else {
                     return;
                 }
                 // change answers text
                 let q_answers = _questions[_current].answers;
                 ui_answers_texts.map((answer, index) => {
                     changeText(ui_answers_texts[index], q_answers[index].answer);
-                })
+                });
                 // change questions text
                 changeText(ui_question_text, _questions[_current].question)
             };
 
             let attachEvents = () => {
-                next.addEventListener("click", () => {changeQuestion(1);});
-                prev.addEventListener("click", () => {changeQuestion(0)});
+                next.addEventListener("click", () => {
+                    changeQuestion(1);
+                });
+                prev.addEventListener("click", () => {
+                    changeQuestion(0)
+                });
 
                 ui_answers.map((answer) => {
-                    answer.addEventListener("click", (e) =>{e.stopPropagation(); console.log(e.target.getAttribute('data-answer-id'))});
+                    answer.addEventListener("click", (e) => {
+                        addAnswers(e);
+                    });
                 })
 
             };
